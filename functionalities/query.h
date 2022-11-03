e6b527ee65ad35d3b89f66be3013752bf805b682
Dipankar <dipankarcode@gmail.com>
2022-11-03 17:39:30 +0530

    Clean Code Submit

--- functionalities/query.h
+++ functionalities/query.h
@@ -4,42 +4,46 @@ using namespace std ;
 
 class Query
 {
-    public : 
-    vector<int> attriblueStatus ;
-    Person target ;
-    bool partialMode = 0 ;
+    const int ON = 1;
+    const int OFF = 0;
+
+public:
+    vector<int> attriblueStatus;
+    Person target;
+    bool partialMode = 0;
 
     Query(int noOfAttribute)
     {
-        attriblueStatus = vector<int>(noOfAttribute + 1 ,0) ;
+        attriblueStatus = vector<int>(noOfAttribute + 1, 0);
     }
 
     void setQueryFrame()
     {
-        cout << "Prefix Search Mode ON(1) : OFF(0) " ;
-        cin >> partialMode ;
+        cout << "Prefix Search Mode ON(1) : OFF(0) ";
+        cin >> partialMode;
 
-        cout << "Apply First Name ?  1 (YES) : 0 (NO)   " ;
-        cin >>  attriblueStatus[1] ;
+        cout << "Apply First Name ?  1 (YES) : 0 (NO)   ";
+        cin >> attriblueStatus[1];
 
-        if(attriblueStatus[1] == 1)
+        if (attriblueStatus[1] == ON)
         {
-            target.setFirstName() ;
+            target.setFirstName();
         }
 
-        cout << "Apply Last Name ?  1 (YES) : 0 (NO)  " ;
-        cin >>  attriblueStatus[2] ;
-        if(attriblueStatus[2] == 1)
+        cout << "Apply Last Name ?  1 (YES) : 0 (NO)  ";
+        cin >> attriblueStatus[2];
+
+        if (attriblueStatus[2] == ON)
         {
-            target.setLastName() ;
+            target.setLastName();
         }
 
-        cout << "Apply Phone Number ? 1(YES) : 0(NO)  " ;
-        cin >> attriblueStatus[3] ;
-        if(attriblueStatus[3] == 1)
+        cout << "Apply Phone Number ? 1(YES) : 0(NO)  ";
+        cin >> attriblueStatus[3];
+        
+        if (attriblueStatus[3] == ON)
         {
-            target.setPhoneNumber() ;
+            target.setPhoneNumber();
         }
     }
-
 };
--- functionalities/search.h
+++ functionalities/search.h
@@ -1,51 +1,59 @@
 #include <bits/stdc++.h>
 #include "query.h"
 #include "stringMatching.h"
-using namespace std ;
-vector<Person> search(vector<Person> groupOfPerson , Query queryFrame)
+using namespace std;
+vector<Person> search(vector<Person> groupOfPerson, Query queryFrame)
 {
-    bool partialSearchApplicable = queryFrame.partialMode ;
-    vector<int>& attriblueStatus = queryFrame.attriblueStatus ;
-    Person targetPerson = queryFrame.target ;
+    bool partialSearchApplicable = queryFrame.partialMode;
+    vector<int> &attriblueStatus = queryFrame.attriblueStatus;
+    Person targetPerson = queryFrame.target;
 
-    vector<Person> quilifiedPersons ;
+    vector<Person> quilifiedPersons;
+
+    const int ON = 1;
+    const int OFF = 0;
 
     for (Person person : groupOfPerson)
     {
-        pair<int,int> quilifiedPerson = { 1 , 1 } ;
+        pair<int, int> quilifiedPerson = {1, 1};
 
-        if (attriblueStatus[1] == 1 )
+        if (attriblueStatus[1] == ON)
         {
+            // First Name
             pair<bool, bool> resultFirstName = compare(targetPerson.getFirstName(), person.getFirstName());
-            quilifiedPerson.first &= resultFirstName.first ;
-            quilifiedPerson.second &= resultFirstName.second ;
+            quilifiedPerson.first &= resultFirstName.first;
+            quilifiedPerson.second &= resultFirstName.second;
         }
 
-        if(attriblueStatus[2] == 1)
+        if (attriblueStatus[2] == ON)
         {
+            // Last Name
             pair<bool, bool> resultLastName = compare(targetPerson.getLastName(), person.getLastName());
-            quilifiedPerson.first &= resultLastName.first ;
-            quilifiedPerson.second &= resultLastName.second ;
+            quilifiedPerson.first &= resultLastName.first;
+            quilifiedPerson.second &= resultLastName.second;
         }
 
-        if(attriblueStatus[3] == 1)
+        if (attriblueStatus[3] == ON)
         {
+            // Phone Numeber
             pair<bool, bool> resultPhone = compare(targetPerson.getPhoneNumber(), person.getPhoneNumber());
-            quilifiedPerson.first &= resultPhone.first ;
-            quilifiedPerson.second &= resultPhone.second ;
+            quilifiedPerson.first &= resultPhone.first;
+            quilifiedPerson.second &= resultPhone.second;
         }
 
-        if(quilifiedPerson.second == 1)
+        if (quilifiedPerson.second == ON)
         {
-            quilifiedPersons.push_back(person) ;
+            // Full Match
+            quilifiedPersons.push_back(person);
             continue;
         }
 
-        else if(partialSearchApplicable and quilifiedPerson.first == 1)
+        else if (partialSearchApplicable and quilifiedPerson.first == ON)
         {
-            quilifiedPersons.push_back(person) ;
+            // Partail Match
+            quilifiedPersons.push_back(person);
         }
     }
 
-    return quilifiedPersons ;
+    return quilifiedPersons;
 };
