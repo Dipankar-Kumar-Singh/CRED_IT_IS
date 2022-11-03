66d71d1571f10dda1e32cec4d40c87995784869e
Dipankar <dipankarcode@gmail.com>
2022-11-03 11:45:40 +0530

    basic Func

--- main.cpp
+++ main.cpp
@@ -1,10 +1,90 @@
-#include <iostream>
 #include <bits/stdc++.h>
+using namespace std ;
+
+
+class PhoneNumber{
+    private : 
+    string countryCode ;
+    string baseNumber ;
+
+    public : 
+
+    PhoneNumber(string _countryCode, string _baseNumber)
+    {
+        countryCode = _countryCode ;
+        baseNumber = _baseNumber ;
+    }
+
+    PhoneNumber() {} ;
+};
+
+
 
 class Person {
+    
+    private : 
+    string firstName ; 
+    string lastName ;
+    PhoneNumber phoneNumber ;
+
+    public :
+    Person(string _firstName , string _lastName , PhoneNumber _phoneNumber ){
+        firstName  = _firstName ;
+        lastName = _lastName ;
+        phoneNumber = _phoneNumber ;
+    } 
+
+    Person(){} ;
+
+    public : 
+    string getFirstName()
+    {
+        return firstName ;
+    }  
+
+    bool matchedName(string name)
+    {
+        if(firstName == name) return true ; 
+        else if(lastName == name) return true ;
+        return false ;
+    }
+
+    bool partialMatched(Person person)
+    {
+        for(int i = 0 ; i < person.getFirst)
+    }
+};
+
+
+class ContactBook{
+
+    vector<Person> groupOfPerson ;
+
+
+    vector<Person> searchByNameExact(string name)
+    {
+        vector<Person> queryResult  ;
+        for(Person& person : groupOfPerson)
+        {
+            if(person.matchedName(name))
+            {
+                queryResult.push_back(person) ;
+            }
+        }  
+
+        return queryResult ; 
+    }
+
+
+    vector<Person> searchByNamePartial(string name)
+    {
+        
+    }
 
 };
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+
+    Person p ;
 }
